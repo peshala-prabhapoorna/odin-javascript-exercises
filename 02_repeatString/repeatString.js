dd5107f877ea2bbd1be93279da75479160b74f84
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-19 00:13:21 +0530

    solution to 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
 
+    let stringOut = '';
+    for (let i = 0; i < num; i++) {
+        stringOut += string;
+    }
+    return stringOut;
 };
 
 // Do not edit below this line
