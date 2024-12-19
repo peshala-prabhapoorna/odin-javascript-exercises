d509d83813c4793055be08bdfa95b8689139b072
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-19 11:38:59 +0530

    solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!(typeof num2 === "number") || !(typeof num1 === "number")) return 'ERROR';
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    if (num1 < 0 || num2 < 0) return 'ERROR';
 
+    const sum1 = num1 * (num1 + 1) / 2;
+    const sum2 = num2 * (num2 + 1) / 2;
+
+    if (sum1 < sum2) return sum2 - sum1 + num1;
+    return sum1 - sum2 + num2;
 };
 
 // Do not edit below this line
