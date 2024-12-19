57b84e54f371c43c3ba8d5267839035cbffeea2c
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-19 07:40:11 +0530

    solution to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reversedStringArray = stringArray.reverse();
+    const reversedString = reversedStringArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
