7cb95f1e8bd7e9a5a9a6c9d4f6be99b75f6195f4
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-19 12:44:15 +0530

    solution to 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isCenturyLeapYear = year % 100 === 0 && year % 400 === 0;
+    const isNonCenturyLeapYear = year % 4 === 0 && year % 100 != 0;
+    if (isCenturyLeapYear || isNonCenturyLeapYear) return true;
+    return false;
 };
 
 // Do not edit below this line
