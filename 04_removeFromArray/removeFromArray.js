b1f22d1c5406c50f47d81d7a67191884b7dd78ce
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-19 10:15:49 +0530

    solution to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const arg of args) {
+        while (true) {
+            const index = array.indexOf(arg);
+            if (index < 0) break;
+            
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
